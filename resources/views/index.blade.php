9b0dfdf9cdcc06ea6727f597cfde8bb2755cf32f
Huy-Pzo <rkaipzo19@gmail.com>
2023-06-09 11:51:56 +0700

    change warning color

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html class="no-js" lang="">
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
@@ -7,42 +8,49 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Favicon -->
-    <link rel="shortcut icon" type="image/x-icon" href="{{asset('public/img/favicon.png')}}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('public/img/favicon.png') }}">
     @yield('header')
+    <style>
+        .bg-danger {
+            background-color: #EE6B6E !important;
+        }
+    </style>
 </head>
 
 <body>
-<!-- Preloader Start Here -->
-<div id="preloader"></div>
-<!-- Preloader End Here -->
-<div id="wrapper" class="wrapper bg-ash">
-    <!-- Header Menu Area Start Here -->
-    @include('components.header')
-    <!-- Header Menu Area End Here -->
-    <!-- Page Area Start Here -->
-    <div class="dashboard-page-one">
-        <!-- Sidebar Area Start Here -->
-        <div class="sidebar-main sidebar-menu-one sidebar-expand-md sidebar-color">
-            <div class="mobile-sidebar-header d-md-none">
-                <div class="header-logo">
-                    <a href="{{URL::to('/trang-chu')}}"><img src="{{asset('public/img/logo1.png')}}" alt="logo"></a>
+    <!-- Preloader Start Here -->
+    <div id="preloader"></div>
+    <!-- Preloader End Here -->
+    <div id="wrapper" class="wrapper bg-ash">
+        <!-- Header Menu Area Start Here -->
+        @include('components.header')
+        <!-- Header Menu Area End Here -->
+        <!-- Page Area Start Here -->
+        <div class="dashboard-page-one">
+            <!-- Sidebar Area Start Here -->
+            <div class="sidebar-main sidebar-menu-one sidebar-expand-md sidebar-color">
+                <div class="mobile-sidebar-header d-md-none">
+                    <div class="header-logo">
+                        <a href="{{ URL::to('/trang-chu') }}"><img src="{{ asset('public/img/logo1.png') }}"
+                                alt="logo"></a>
+                    </div>
                 </div>
+                @include('components.menu')
             </div>
-            @include('components.menu')
-        </div>
 
-        <!-- Sidebar Area End Here -->
-        <div class="dashboard-content-one">
-            <!-- Breadcubs Area Start Here -->
-            <div class="breadcrumbs-area">
+            <!-- Sidebar Area End Here -->
+            <div class="dashboard-content-one">
+                <!-- Breadcubs Area Start Here -->
+                <div class="breadcrumbs-area">
+                </div>
+                <!-- Breadcubs Area End Here -->
+                @yield('contents')
+
             </div>
-            <!-- Breadcubs Area End Here -->
-            @yield('contents')
-
         </div>
+        <!-- Page Area End Here -->
     </div>
-    <!-- Page Area End Here -->
-</div>
-@yield('footer')
+    @yield('footer')
 </body>
+
 </html>
