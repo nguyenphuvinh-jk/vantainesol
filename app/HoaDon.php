127a4e8f74e24a39ff98c27456aac83376e6af01
Huy-Pzo <rkaipzo19@gmail.com>
2023-06-09 11:35:57 +0700

    add property 'dathanhtoan'

--- app/HoaDon.php
+++ app/HoaDon.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class HoaDon extends Model
 {
-    protected $fillable = ['hoadon_id','phithuexe', 'phinang',	'phiha',	'phigiayto',	'philuuca',	'vecauduong',	'phikhac',	'trangthai',	'thue',	'donhang_id',	'tongtien',	'ghichu', 'created_at'];
+    protected $fillable = ['hoadon_id','phithuexe', 'phinang',	'phiha',	'phigiayto',	'philuuca',	'vecauduong',	'phikhac',	'trangthai',	'thue',	'donhang_id',	'tongtien',	'ghichu', 'created_at' , 'dathanhtoan'];
     protected $primaryKey='hoadon_id';
     protected $table = 'tbl_hoadon';
 }
